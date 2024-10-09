e6769ddd0b409d898558c1eb9298edd907e17473
Gustavo Henrique <105958604+xdghlw@users.noreply.github.com>
2024-10-09 10:18:35 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/mtp2024/Mtp2024.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mtp2024;
+
+/**
+ *
+ * @author 20241070130015
+ */
+public class Mtp2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
